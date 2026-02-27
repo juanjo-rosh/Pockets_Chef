a0a34f65719fc54e4cbb0f869024776bb3032291
Juan Jose Rosales Hernando <juanjorosales2004@gmail.com>
2026-02-27 13:09:49 +0100

    `First Commit`

---
+++ app/src/main/java/es/uc3m/android/pockets_chef_app/MainActivity.kt
@@ -0,0 +1,47 @@
+package es.uc3m.android.pockets_chef_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import es.uc3m.android.pockets_chef_app.ui.theme.Pockets_Chef_AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pockets_Chef_AppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pockets_Chef_AppTheme {
+        Greeting("Android")
+    }
+}
